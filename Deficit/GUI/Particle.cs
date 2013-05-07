3dea62af6e8b6e9906f7a359a6b094f1f2c0d5c2
Alex Bardanov <dnbard@gmail.com>
2013-05-08 00:56:53 +0300

    particles

---
+++ Deficit/GUI/Particle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Deficit.core;
+using Microsoft.Xna.Framework;
+
+namespace Deficit.GUI
+{
+    class Particle: ParallaxComponent
+    {
+        public int FrameCount { get; set; }
+        protected int CurrentFrame = 0;
+
+        public Particles Parent;
+
+        public bool Repeat { get; set; }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (ParallaxValue >= 0)
+            {
+                int mX = MouseManager.X;
+                float maxX = _viewport.X;
+
+                x = (int) (mX/maxX*ParallaxValue)*Direction + X;
+            }
+
+            CurrentFrame++;
+            if (CurrentFrame < FrameCount) return;
+            if (Repeat) CurrentFrame = 0;
+            else if (Parent != null) Parent.Remove(this);
+        }
+
+        protected string FrameKey
+        {
+            get { return string.Format("{0}{1}", TextureKey, CurrentFrame); }
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            if (Texture == null) return;
+            //Texture.Draw(_batch, TextureKey, x, Y, Color.White, Layer);
+            Texture.Draw(_batch, FrameKey, x, Y, Color.White, Layer + CurrentFrame * 0.0001f);
+        }
+    }
+}
---
+++ Deficit/GUI/Particles.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Deficit.Images;
+using Microsoft.Xna.Framework;
+
+namespace Deficit.GUI
+{
+    class Particles : DrawableGameComponent
+    {
+        private readonly List<Particle> particles = new List<Particle>(600);
+
+        public Particles() : base(Program.Game)
+        {
+            
+        }
+
+        public void Remove(Particle p)
+        {
+            if (particles.Contains(p)) particles.Remove(p);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            int i = 0;
+            while (i < particles.Count)
+            {
+                particles[i].Update(gameTime);
+                i++;
+            }
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            foreach (var particle in particles)
+                particle.Draw(gameTime);
+        }
+
+        public void AddTrails(int x, int y)
+        {
+            var particle = new Particle
+                {
+                    X = x,
+                    Y = y,
+                    ParallaxValue = 0,
+                    Texture = ImagesManager.Get("gfx-hit"),
+                    TextureKey = "firespark",
+                    Layer = 0.75f
+                };
+            particles.Add(particle);
+        }
+    }
+}
