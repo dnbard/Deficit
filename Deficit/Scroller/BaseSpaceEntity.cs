d91a51d597bfb343bfd424c0d84a535fdab99068
Alex Bardanov <dnbard@gmail.com>
2013-06-08 02:17:18 +0300

    space entity events model

--- Deficit/Scroller/BaseSpaceEntity.cs
+++ Deficit/Scroller/BaseSpaceEntity.cs
@@ -8,7 +8,7 @@ using Microsoft.Xna.Framework;
 
 namespace Deficit.Scroller
 {
-    class BaseSpaceEntity:VisualComponent
+    class BaseSpaceEntity : VisualComponent
     {
         protected Vector2 Origin;
         public float LinearSize { get; set; }
@@ -17,16 +17,72 @@ namespace Deficit.Scroller
 
         protected float SpeedInTick { get { return LinearSpeed/60f; } }
 
+        public float AngularSpeed { get; set; }
 
         public Action<BaseSpaceEntity, BaseSpaceEntity> OnCollision { get; set; }
         public Action<BaseSpaceEntity> OnTime { get; set; }
 
         public SceneScroller ParentScene { get; set; }
 
+        public EntityAlignment Alignment { get; protected set; }
+
         public BaseSpaceEntity()
         {
             LinearSpeed = 0f;
+            Damage = 1;
+            PositionChanged += SpaceEntityPositionChanged;
+
+            Health = MaxHealth = 1;
         }
+
+        protected bool isVisible;
+
+        public Action<VisualComponent> MoveOutViewport { get; set; }
+        public Action<VisualComponent> MoveInViewport { get; set; }
+
+        private void SpaceEntityPositionChanged(VisualComponent entity)
+        {
+            var viewport = Program.Game.Viewport;
+            var size = entity.Size;
+            var visible = entity.X + size.X >= 0 && entity.X - size.X <= viewport.X &&
+                        entity.Y + size.Y >= 0 && entity.Y - size.Y <= viewport.Y;
+
+            if (!visible && isVisible && MoveOutViewport != null) //was visible, but not right now
+                MoveOutViewport(this);
+            else if (visible && !isVisible && MoveInViewport != null) //was out of viewport, but not right now
+                MoveInViewport(this);
+
+            isVisible = visible;
+        }
+
+        public int Damage { get; set; }
+
+        public bool IsDamaged { get { return Health < MaxHealth; } }
+
+        private int _health;
+        public int Health
+        {
+            get { return _health; }
+            set
+            {
+                if (value < _health && OnDamage != null) OnDamage(this);
+                _health = value;
+
+                if (value <= 0)
+                {
+                    //target is destroyed
+                    if (OnDestroy != null) OnDestroy(this);
+                    ParentScene.Remove(this);
+                }
+
+                if (value > MaxHealth) MaxHealth = value;
+            }
+        }
+
+        public int MaxHealth { get; protected set; }
+
+        protected Action<BaseSpaceEntity> OnDestroy { get; set; }
+        protected Action<BaseSpaceEntity> OnDamage { get; set; }
 
         public override string TextureKey
         {
@@ -55,6 +111,12 @@ namespace Deficit.Scroller
             if (OnTime != null) OnTime(this);
 
             if (ParentScene == null) ParentScene = SceneManager.Get("scroller") as SceneScroller;
+
+            if (AngularSpeed != 0f)
+            {
+                Rotation += AngularSpeed;
+                if (Math.Abs(Rotation) > Math.PI*2) Rotation = 0;
+            }
         }
 
         public override void Draw(GameTime gameTime)
@@ -81,6 +143,15 @@ namespace Deficit.Scroller
                     if (entity.OnCollision != null) entity.OnCollision(entity, this);
                 }
             }
+        }
+
+        /// <summary>
+        /// Remove object from all containers quietly without effects
+        /// </summary>
+        /// <param name="entity"></param>
+        protected void RemoveQuietly(VisualComponent entity)
+        {
+            SceneManager.RemoveElement(entity);
         }
     }
 }
---
+++ Deficit/Scroller/EntityAlignment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Deficit.Scroller
+{
+    public enum EntityAlignment
+    {
+        /// <summary>
+        /// Do not be added to containers
+        /// </summary>
+        Neutral,
+        Hostile,
+        Friendly
+    }
+}
